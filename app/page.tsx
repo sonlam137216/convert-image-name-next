4b6b5ac58cefe0c26c20df3dcb33b4e5ee1208b0
sonlt <sonlam@gmail.com>
2024-08-22 00:10:15 +0700

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,80 @@
-import { Metadata } from "next"
-import { Button } from "components/Button/Button"
-import { LP_GRID_ITEMS } from "lp-items"
+"use client";
+// import { Metadata } from "next"
+import { useState } from "react";
 
-export const metadata: Metadata = {
-  title: "Next.js Enterprise Boilerplate",
-  twitter: {
-    card: "summary_large_image",
-  },
-  openGraph: {
-    url: "https://next-enterprise.vercel.app/",
-    images: [
-      {
-        width: 1200,
-        height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
-      },
-    ],
-  },
-}
+// export const metadata: Metadata = {
+//   title: "Next.js Image Path Generation",
+//   twitter: {
+//     card: "summary_large_image",
+//   },
+//   openGraph: {
+//     url: "https://next-enterprise.vercel.app/",
+//     images: [
+//       {
+//         width: 1200,
+//         height: 630,
+//         url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
+//       },
+//     ],
+//   },
+// }
 
 export default function Web() {
+
+  // State to hold the input value
+  const [inputText, setInputText] = useState('');
+
+  // Function to handle input change
+  const handleInputChange = (e: any) => {
+    // Remove spaces from the input value
+    const transformedText = e.target.value.normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .replace('-', '')
+    .replace(/\s{2,}/g, ' ').split(' ').join('-')
+    setInputText(transformedText);
+  };
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(inputText)
+      .then(() => {
+        alert("Text copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
+      });
+  };
+
   return (
     <>
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
+            Next.js Image Path Generation
             </h1>
-            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-              Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
-              Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
-              enjoyable development process.
-            </p>
-            <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
-              Get started
-            </Button>
-            <Button
-              href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
-              intent="secondary"
-            >
-              Deploy Now
-            </Button>
+          <div className="relative mt-2 rounded-md shadow-sm">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <span className="text-gray-500 sm:text-sm"></span>
+            </div>
+            <input 
+              type="text" 
+              name="price" 
+              id="price" 
+              className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+              placeholder="Iu bạn Lu nhiều ạ" 
+              onChange={handleInputChange}
+              />
           </div>
-        </div>
-      </section>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6">
-          <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
-            {LP_GRID_ITEMS.map((singleItem) => (
-              <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
-                <div className="mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-100 p-1.5 text-blue-700 dark:bg-primary-900 lg:h-12 lg:w-12">
-                  {singleItem.icon}
-                </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
-                <p className="text-gray-500 dark:text-gray-400">{singleItem.description}</p>
-              </div>
-            ))}
+          <div className="mt-4 p-5 rounded-md bg-slate-100 text-lg font-medium text-gray-700 dark:text-gray-300">
+            {inputText}
+          </div>
+
+          <button 
+            onClick={handleCopyClick} 
+            className="mt-4 px-4 py-2 rounded-md bg-indigo-600 text-white hover:bg-indigo-700">
+            Copy Text
+          </button>
           </div>
         </div>
       </section>
